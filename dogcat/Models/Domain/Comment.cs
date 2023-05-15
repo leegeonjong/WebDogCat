88e27d49598f089bb9bfbca58826032aecc41c33
lateaksoo <ted26@naver.com>
2023-05-15 17:32:05 +0900

    주석추가

--- dogcat/Models/Domain/Comment.cs
+++ dogcat/Models/Domain/Comment.cs
@@ -5,7 +5,7 @@ namespace dogcat.Models.Domain
     [Table("Comment")]
     public class Comment
     {
-        public long Id { get; set; }  // PK
+        public long Id { get; set; }  // PK //주석 추가
         public string Content { get; set; }  // 댓글 내용
         public DateTime Time { get; set; }  // 댓글 작성시간
 
