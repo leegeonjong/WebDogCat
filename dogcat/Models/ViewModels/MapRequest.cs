2fc41807fd7dd8902c25b5d887bee38b49e34448
lateaksoo <ted26@naver.com>
2023-05-16 11:41:45 +0900

    ViewModels

---
+++ dogcat/Models/ViewModels/MapRequest.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace dogcat.Models.ViewModels
+{
+    public class MapRequest : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
