f9cb990cb4d1547d9b2cf0db1413efc42bfc7016
jryong96 <jryoung96@gmail.com>
2023-05-17 09:49:57 +0900

    User_Repository

---
+++ dogcat/Repositories/IUserRepositories.cs
@@ -0,0 +1,18 @@
+﻿
+using dogcat.Models.Domain;
+
+namespace dogcat.Repositories
+{
+    public interface IUserRepositories
+    {
+        //로그인 시 회원정보 가져오기
+        Task<User?> GetInfo(long  id);
+        //회원가입
+        Task<User> AddUser(User user);
+        //회원 탈퇴
+        Task<User?> DeleteUser(long id);
+        //회원정보 수정
+        Task<User> UpdateUser(long id);
+
+    }
+}
