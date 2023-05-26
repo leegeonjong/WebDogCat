0422431eb7135bfa16efcb8b11168e343767ef60
com7415 <qazx4740@naver.com>
2023-05-26 17:22:06 +0900

    검색 기능 해줘

--- dogcat/wwwroot/js/Admin/search.js
+++ dogcat/wwwroot/js/Admin/search.js
@@ -1,14 +1,1 @@
-﻿$(function () {
-    $("#searchbtn").click(function () {
-        let answer = confirm("밴하시겠습니까?");
-        if (answer) {
-            $("form[name='frmban']").submit();
-        }
-    });
-    $("#noban").click(function () {
-        let answer = confirm("밴을 해제하시겠습니까?");
-        if (answer) {
-            $("form[name='frmban']").submit();
-        }
-    })
-});
+﻿
